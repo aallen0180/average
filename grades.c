edcbea446eb606fc74e52297871667098b9d77b4
aallen0180 <92514293+aallen0180@users.noreply.github.com>
2021-10-21 11:24:17 +0530

    Create grades.c

---
+++ grades.c
@@ -0,0 +1,27 @@
+include<stdio.h>
+ int main()
+ {
+ 	int marks;
+ 	printf("enter the mark");
+ 	if(average>=85)
+ {
+ 	printf("grade A");
+ }
+	else if(average>=70)
+ {
+ 	printf("grade B");
+ }
+ 	else if(average>=55)
+ {
+ 	printf("grade C");
+ }
+    else if(average<=40)
+ {
+ 	printf("grade D");
+ }
+    else if(average>=40)
+ {
+ 	printf("grade F");
+ }
+    return 0; 
+ }
